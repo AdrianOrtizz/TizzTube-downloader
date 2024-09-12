814f6e6d92d98bc4cffaff4dcd35a58c925bc099
QuemeraT <adrianortizzt1vl@gmail.com>
2024-09-11 22:05:52 -0300

    Creacion del archivo para importar fuentes

---
+++ src/utils/fonts/Fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Oooh_Baby } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const ooohBaby = Oooh_Baby({ weight: "400", subsets: ["latin"] });
